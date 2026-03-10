876a447f657227d326c4255ffc295e60048cc841
Sonni17 <sonyaeasanchez@outlook.com>
2026-03-10 05:06:19 -0400

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+let radius = 7;
+const pi = 3.14;
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
+let area = radius * radius * pi;
+console.log(area);
+
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
